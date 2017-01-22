4b52c94dc2b16ec0aa41b4f9dd7a7ca90f8f9050
sharonpvs <sharonpvs@hotmail.com>
2017-01-22 11:55:33 -0800

    image and text align
    separated different mountains labels in separate layouts.
    image displays with text over top.

--- SnowNote/app/src/main/java/com/example/pvs/snownote/MainActivity.java
+++ SnowNote/app/src/main/java/com/example/pvs/snownote/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.pvs.snownote;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //load image resource from drawable
+        Bitmap bmap = BitmapFactory.decodeResource(getResources(), R.drawable.titletop);
+        //Bitmap bMapScaled = Bitmap.createScaledBitmap(bmap, 800, 400, true);
+
+        ImageView image = (ImageView) findViewById(R.id.startlogo);
+        image.setImageBitmap(bmap);
     }
 }
